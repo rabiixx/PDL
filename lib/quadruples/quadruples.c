71b9cc076d36fa7a95d56c42079419f97d4fe040
rabiixx <rabiixx@gmail.com>
2020-10-23 06:30:44 -0400

    quadruples v0.3

--- lib/quadruples/quadruples.c
+++ lib/quadruples/quadruples.c
@@ -20,22 +20,22 @@ QuadTable *new_quad_tb() {
 Quad *new_quad(const int op, const int op1, const unsigned int op2, const unsigned int res) {
 
 
-	Quad q = (Quad*) calloc(1, sizeof( Quad ));
+	Quad *quad = (Quad*) calloc(1, sizeof( Quad ));
 
-	if ( qt == NULL )
+	if ( quad == NULL )
 	{
 		perror("Impossible to assign memory to quad");
 		exit(-1);
 	}
 
 	/* Check if given op is valid (check enum range) */
-	q->op = op;
-	q->op1 = op1;
-	q->op2 = op2;
-	q->res = res;
-	q->next = NULL;
+	quad->op = op;
+	quad->op1 = op1;
+	quad->op2 = op2;
+	quad->res = res;
+	quad->next = NULL;
 
-	return q;
+	return quad;
 
 }
 
@@ -81,6 +81,25 @@ LinkedList *new_linked_list() {
 
 }
 
+void insert_node( LinkedList *list, Node *node ) {
+
+
+	if ( !(list->head) )
+	{
+	 	list->head = list->tail = node; 
+	}
+	else
+	{
+		printf("sadas\n");
+
+		list->tail->next = node;
+		list->tail = node;
+	}
+
+	++(list->size);
+
+}
+
 LinkedList *makelist( Quad *quad ) {
 
 	LinkedList *ll = new_linked_list();
@@ -92,53 +111,114 @@ LinkedList *makelist( Quad *quad ) {
 
 }
 
+
+/**
+  * Merges two given lists into new one (non-destructive)
+  *
+  */
+
+LinkedList *merge( LinkedList *l1, LinkedList *l2 ) {
+	
+	if ( !l1 && !l2 )
+	{
+		return NULL;
+	}
+	else 
+	{
+		
+		LinkedList *list = new_linked_list();
+		Node *tmp;
+
+		tmp = l1->head;
+
+		while ( tmp ) {
+			Node *node = new_node( tmp->q );
+			insert_node(list, node);
+		}
+
+		tmp = l2->head;
+
+		while ( tmp ) {
+			Node *node = new_node( tmp->q );
+			insert_node(list, node);
+		}
+
+		return list;
+	}
+}
+
+
+
+
 int main(int argc, char const *argv[])
 {
 	
-
 	QuadTable *qt = new_quad_tb();
+	
+	Quad *quad1 = new_quad(99, 1, 2 , 3);
+
+	printf("quad1: \n");
+	printf("\top: %d\n", quad1->op);
+	printf("\top1: %d\n", quad1->op1);
+	printf("\top2: %d\n", quad1->op2);
+	printf("\tres: %d\n", quad1->res);
+
+
+	Quad *quad2 = (Quad*) calloc(1, sizeof( Quad ) );
+	*quad2 = *quad1;
+
+	printf("MA quad1: %p\n", quad1);
+	printf("MA quad2: %p\n", quad2);
+
+	printf("quad2: \n");
+	printf("\top: %d\n", quad2->op);
+	printf("\top1: %d\n", quad2->op1);
+	printf("\top2: %d\n", quad2->op2);
+	printf("\tres: %d\n", quad2->res);
+
+	quad2->op = 100;
+
+	printf("quad1: \n");
+	printf("\top: %d\n", quad1->op);
+	printf("\top1: %d\n", quad1->op1);
+	printf("\top2: %d\n", quad1->op2);
+	printf("\tres: %d\n", quad1->res);
+
+	printf("quad2: \n");
+	printf("\top: %d\n", quad2->op);
+	printf("\top1: %d\n", quad2->op1);
+	printf("\top2: %d\n", quad2->op2);
+	printf("\tres: %d\n", quad2->res);
+
+
+	LinkedList *list = new_linked_list();
+	
+	Node *node1 = new_node(quad1);
+	Node *node2 = new_node(quad2);
+
+	insert_node(list, node1);
+	insert_node(list, node2);
+	
+
+	Node *tmp = list->head; 
+
+	int i = 0;
+	while ( tmp ) {
+
+		printf("list[%d]: \n", i);
+
+		printf("\top: %d\n", tmp->q->op);
+		printf("\top1: %d\n", tmp->q->op1);
+		printf("\top2: %d\n", tmp->q->op2);
+		printf("\tres: %d\n", tmp->q->res);
+
+		tmp = tmp->next;
+		++i;
+	}
+
 
 	return 0;
 }
 
 
 
-LinkedList *merge( LinkedList *l1, LinkedList *l2 ) {
-
-	if ( !l1 && !l2)
-	{
-		return NULL;
-	} 
-	else if ( !l1 )
-	{
-		return l2;
-	} 
-	else if ( !l2 )
-	{
-		return l1;
-	} 
-	else 
-	{
-		
-		LinkedList *l3 = new_linked_list();
-		Node *tmp = (Node*) calloc(1, sizeof( Node ));
-		
-		tmp = l1->head;
-
-
-
-		while ( tmp ) {
-
-		
-
-		}
-
-
-	}
-
-
-
-
-
-
-}
--- lib/quadruples/quadruples.h
+++ lib/quadruples/quadruples.h
@@ -4,24 +4,24 @@
 
 //https://steemit.com/utopian-io/@drifter1/writing-a-simple-compiler-on-my-own-combine-flex-and-bison
 
-typedef struct quadruple {
+typedef struct _quad {
 	operators op;			/* Operators enum */
 	int op1;
 	int op2;
 	int res;
-	struct quead *queadnext; 
+	struct _quad *next; 
 } Quad;
 
 typedef struct _QuadTable {
-	struct quead *head;
-	struct quead *tail;
+	Quad *head;
+	Quad *tail;
 	unsigned int size;
 } QuadTable;
 
 
 typedef struct _node {
 	Quad *q;
-	struct Node *next; 
+	struct _node *next; 
 } Node;
 
 
@@ -37,8 +37,15 @@ QuadTable *new_quad_tb();
 
 Quad *new_quad(const int op, const int op1, const unsigned int op2, const unsigned int res);
 
+void gen(QuadTable *qt, Quad *quad);
+
 Node *new_node( Quad *quad );
 
 LinkedList *new_linked_list();
 
+void insert_node( LinkedList *list, Node *node );
+
 LinkedList *makelist( Quad *quad );
+
+LinkedList *merge( LinkedList *l1, LinkedList *l2 );
+
